a2651983424849ad4288246ca3efe1b6b8f39545
Phraechanok R <64015512kmitl.ac.th>
2021-11-21 19:12:05 +0700

    Create uni_save.py

---
+++ uni_save.py
@@ -0,0 +1,11 @@
+import json
+
+
+def save_data(target_dict, target_file):
+    with open(target_file, "w") as f:
+        json.dump(target_dict, f)
+        f.close()
+
+def load_data(target_file):
+    with open(target_file, "r") as fr:
+        return json.load(fr)
